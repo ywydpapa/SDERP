13e049711400d5c11bacfe0ceb07aeb3d62ff648
RussKim <yunsubin481@gmail.com>
2024-06-27 21:28:01 +0900

    Revert "change: 이미지 출력 방식 정정 (#12)"
    This reverts commit 5c8855704a0a5f1c0349025c341a1541bfcdd2f8.

--- src/main/java/kr/swcore/sderp/util/service/UtilService.java
+++ src/main/java/kr/swcore/sderp/util/service/UtilService.java
@@ -1,13 +1,9 @@
 package kr.swcore.sderp.util.service;
 
-import java.util.List;
-
 import kr.swcore.sderp.util.dto.ImageDTO;
 
 public interface UtilService {
 	int insertImage(ImageDTO dto);
 
-	String getBase64FromServer(String uuid);
-
-	List<String> extractUuidsFromHtml(String htmlCode);
+	String getBase64(String uuid);
 }
